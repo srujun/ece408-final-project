7e4ebacce5ce787b570c69c7d764f42d6fd9865a
Srujun Thanmay Gupta <theununtrium@gmail.com>
2017-11-28 22:58:16 -0600

    Basic milestone3 implementation

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -10,9 +10,10 @@ namespace op
 {
 
 
-
-
-__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K) {
+__global__ void forward_kernel(float *y, const float *x, const float *k,
+                               const int B, const int M, const int C,
+                               const int H, const int W, const int K)
+{
 
     /*
     Modify this function to implement the forward pass described in Chapter 16.
@@ -20,22 +21,46 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     The goal here is to be correct AND fast.
     We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
     */
+    // const int H_out = H - K + 1;
+    // const int W_out = W - K + 1;
+    // #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
+    // #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
+    // #define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
 
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+    // const int batches = B;
+    const int fmaps = M;
+    const int channels = C;
+    const int img_height = H - K + 1;
+    const int img_width = W - K + 1;
+    const int kernel_size = K;
 
-    // An example use of these macros:
-    // float a = y4d(0,0,0,0)
-    // y4d(0,0,0,0) = a
-    #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
+    #define y4d(i3,i2,i1,i0) y[(i3) * (M * img_height * img_width) + (i2)*(img_height * img_width) + (i1)*(img_width) + i0]
     #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
     #define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
 
     /*
         Your code here!
     */
+    int batch = blockIdx.x;
+    int fmap = blockIdx.y;
+    int xval = threadIdx.x;
+    int yval = threadIdx.y;
+
+    float acc = 0;
+
+    for (int channel = 0; channel < channels; ++channel)
+    {
+        for (int k_y = 0; k_y < kernel_size; ++k_y)
+        {
+            for (int k_x = 0; k_x < kernel_size; ++k_x)
+            {
+                acc += x4d(batch, channel, yval + k_y, xval + k_x) *
+                       k4d(fmap, channel, k_y, k_x);
+            }
+        }
+    }
+
+    y4d(batch, fmap, yval, xval) = acc;
 
     #undef y4d
     #undef x4d
@@ -43,33 +68,58 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 }
 
 
-
-
-/* 
+/*
    This function is called by new-inl.h
    Any code you write should be executed by this function.
    For ECE408, we only expect the float version of the operator to be called, so here we specialize with only floats.
 */
 template<>
-void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &w) {
-    
-
+void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y,
+                         const mshadow::Tensor<gpu, 4, float> &x,
+                         const mshadow::Tensor<gpu, 4, float> &w)
+{
     // Use mxnet's CHECK_EQ to do assertions.
-    // Remove this assertion when you do your implementation!
-    CHECK_EQ(0, 1) << "Missing an ECE408 GPU implementation!";
 
     // You'll probably need to launch kernels against the right stream to keep MXNet happy
-    // cudaStream_t s = y.stream_->stream_;
+    cudaStream_t s = y.stream_->stream_;
 
-    // Extract the tensor dimensions into B,M,C,H,W,K
-    // ...
+    // Extract the tensor dimensions
+    const int batches = x.shape_[0];
+    const int fmaps = y.shape_[1];
+    const int channels = x.shape_[1];
+    const int img_height = x.shape_[2];
+    const int img_width = x.shape_[3];
+    const int kernel_size = w.shape_[3];
+
+    fprintf(stdout, "batches = %d\n", batches);
+    fprintf(stdout, "fmaps = %d\n", fmaps);
+    fprintf(stdout, "channels = %d\n", channels);
+    fprintf(stdout, "img_height = %d\n", img_height);
+    fprintf(stdout, "img_width = %d\n", img_width);
+    fprintf(stdout, "kernel_size = %d\n", kernel_size);
 
     // Set the kernel dimensions
     // dim3 gridDim(0);
     // dim3 blockDim(0);
 
+    const int BLOCK_SIZE = 16;
+    const int grid_width = (img_width - kernel_size + 1);// / BLOCK_SIZE;
+    const int grid_height = (img_height - kernel_size + 1);// / BLOCK_SIZE;
+
+    dim3 dimGrid(batches, fmaps, 1);
+    dim3 dimBlock(grid_width, grid_height, 1);
+
+    fprintf(stdout, "\n");
+    fprintf(stdout, "grid(%d, %d, %d)\n", dimGrid.x, dimGrid.y, dimGrid.z);
+    fprintf(stdout, "block(%d, %d, %d)\n", dimBlock.x, dimBlock.y, dimBlock.z);
+
     // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    // forward_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+    forward_kernel<<<dimGrid, dimBlock, 0, s>>>(
+        y.dptr_, x.dptr_, w.dptr_,
+        batches, fmaps, channels,
+        img_height, img_width, kernel_size
+    );
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
@@ -77,7 +127,7 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 }
 
 
-/* 
+/*
     This tells mxnet how to do an op when it's not a float.
     This is not used in the ECE408 project
 */
